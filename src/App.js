2a4925d3fa83010271931126ddd22960c51c122e
Oleg Kolesnikov <ok9168075249@gmail.com>
2022-05-12 13:38:37 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+// Contains all of the app components
+
+function App() {
+    return
+    <div>
+        <h1>My First React app</h1>;
+    </div> 
+    
+  }
+  
+  export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// contains the entry point for the application
+
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
