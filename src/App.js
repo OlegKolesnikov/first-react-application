374d8186ede1aad2a0eb85bb6808c4cfb8779a1a
Oleg Kolesnikov <ok9168075249@gmail.com>
2022-05-13 14:13:01 -0700

    added symple css styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 
 // Contains all of the app components
+// npm run dev
+
 
 function App() {
-    return
-    <div>
-        <h1>My First React app</h1>;
-    </div> 
-    
+    return <div class="my-background" >
+        <h1>My First React app</h1>
+
+            <ol>
+                <li> blue green
+                    <div class ="my-blue">
+                        <div class ="my-green-circle" ></div>
+                    </div>
+                </li>
+
+                <li>yellow red
+                    <div class ="my-yellow" >
+                        <div class ="my-red-circle"></div>
+                    </div >
+                </li>
+
+                <li>green yellow
+                    <div class ="my-green">
+                        <div class ="my-yellow-circle"></div>
+                    </div>
+                </li>
+            </ol>
+       
+           
+        </div>
+        
   }
+  
   
   export default App;
